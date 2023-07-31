82989eb251bb7b0096e0b0ab476f001223c59215
Asude Gürkan <61904935+asudegrkn@users.noreply.github.com>
2023-07-31 10:52:31 +0300

    add final version.
    final version of the project.

---
+++ model/attendance.py
@@ -0,0 +1,79 @@
+import datetime
+
+
+class Attendance:
+    def __int__(self, conn):
+        # Veritabanı bağlantısı
+        self.conn = conn
+
+    def get_lesson_name(self):
+        # O anki güncel gün ve saat bilgisi
+        current_day = datetime.datetime.today().weekday() + 1
+        current_time = datetime.datetime.now().time()
+
+        # Veritabanı bağlantısını oluşturun
+        cursor = self.conn.cursor()
+
+        # SQL sorgusu
+        query = "SELECT lesson_id FROM schedule WHERE day = %s AND start_time <= %s AND end_time >= %s"
+        cursor.execute(query, (current_day, current_time, current_time))
+        result = cursor.fetchone()
+
+        lesson_id = result[0]
+        
+        if lesson_id == 1:
+            lesson_name = "ders1"
+        elif lesson_id == 2:
+            lesson_name = "ders2"
+        elif lesson_id == 3:
+            lesson_name = "ders3"
+        elif lesson_id == 4:
+            lesson_name = "ders4"
+        elif lesson_id == 5:
+            lesson_name = "ders5"
+        elif lesson_id == 6:
+            lesson_name = "ders6"
+        elif lesson_id == 7:
+            lesson_name = "ders7"
+        elif lesson_id == 8:
+            lesson_name = "ders8"
+        elif lesson_id == 9:
+            lesson_name = "ders9"
+        elif lesson_id == 10:
+            lesson_name = "ders10"
+        else:
+            print("No lesson scheduled at the moment.")
+            return
+
+        return lesson_name
+
+
+    def create_attendance(self,name,lesson_name):
+        # İsimdeki boşluk karakterine göre ayrıştırma
+        first_name, last_name, number = name.split(" ")
+
+        # Anlık tarihin alınması
+        current_date = datetime.date.today()
+
+        # Veritabanı bağlantısını oluşturun
+        cursor = self.conn.cursor()
+
+        # Katılım kaydının var olup olmadığını kontrol etme
+        cursor.execute("SELECT * FROM {} WHERE date = %s AND student_no = %s".format(lesson_name),
+                   (current_date, number))
+        existing_attendance = cursor.fetchone()
+
+        if existing_attendance:
+            return
+        else:
+            # Veritabanına katılım kaydının eklenmesi
+            cursor.execute("INSERT INTO {} (student_no, date, student_name, student_surname) VALUES (%s, %s, %s, %s)".format(lesson_name),
+                        (number, current_date, first_name, last_name))
+
+            # Veritabanı işlemlerinin kaydedilmesi
+            self.conn.commit()
+
+            print("Attendance created for:", name)
+
+        # Veritabanı bağlantısının kapatılması
+        cursor.close()
---
+++ model/main_video.py
@@ -0,0 +1,95 @@
+import cv2
+from simple_facerec import SimpleFacerec
+from attendance import Attendance
+from register import Register
+import psycopg2
+import requests
+import tempfile
+import shutil
+from urllib.parse import unquote
+import os
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="attendace",
+    user="postgres",
+    password="123456"
+)
+
+# Veritabanı bağlantısını oluşturun
+cursor = conn.cursor()
+
+# Veritabanından fotoğraf URL'lerini çekme
+cursor.execute("SELECT photo FROM student")
+photo_urls = cursor.fetchall()
+
+# Geçici dosyaları tutmak için bir klasör oluşturun
+temp_dir = tempfile.mkdtemp()
+
+# Fotoğrafları indirip geçici dosyalara kaydedin
+for url in photo_urls:
+    # URL'den dosya adını alma
+    filename = unquote(url[0].split("/")[-1])
+
+    # Geçici dosya yolu
+    temp_file_path = os.path.join(temp_dir, filename)
+
+    #url'in doğruluğunu komtrol etmek için HTTTPerror kontrolü'
+    response = requests.get(url[0])
+    response.raise_for_status()
+
+    # Dosyayı geçici klasöre kaydetme
+    with open(temp_file_path, "wb") as file:
+        file.write(response.content)
+
+    # İndirilen dosyanın yolunu yazdırma
+    print("Downloaded image:", temp_file_path)
+
+# Encode faces from a folder
+sfr = SimpleFacerec()
+sfr.load_encoding_images(temp_dir)
+
+# Encode attendance file
+atd = Attendance()
+atd.__int__(conn)
+lesson_name = atd.get_lesson_name()
+print(lesson_name)
+
+reg = Register()
+
+# Load Camera
+cap = cv2.VideoCapture(0)
+
+
+while True:
+    ret, frame = cap.read()
+
+    # Detect Faces
+    face_locations, face_names = sfr.detect_known_faces(frame)
+    for face_loc, name in zip(face_locations, face_names):
+        y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
+
+        cv2.putText(frame, "fotograf cekmek icin q bas", (10, 450), cv2.FONT_HERSHEY_DUPLEX, 1, (200, 0, 200), 1)
+        cv2.putText(frame, name, (x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 4)
+
+        cv2.imshow("Frame", frame)
+
+
+        if name != 'Unknown':
+            atd.create_attendance(name,lesson_name)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        reg.create_window(frame)
+
+    if cv2.waitKey(1) == ord('e'):
+        break
+
+# Geçici klasörü silme
+shutil.rmtree(temp_dir)
+
+cap.release()
+cv2.destroyAllWindows()
+
+# Veritabanı bağlantısını kapatma
+conn.close()
